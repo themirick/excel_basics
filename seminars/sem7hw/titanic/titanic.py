6560e232426ccb7bc9103853047676b8d88b9793
themirick <themirick@gmail.com>
2024-01-10 20:39:02 +0500

    done seminar 7, doing macroses through python

--- seminars/sem7hw/titanic/titanic.py
+++ seminars/sem7hw/titanic/titanic.py
@@ -3,7 +3,7 @@ import xlwings as xw
 
 def main():
     wb = xw.Book.caller()
-    sheet = wb.sheets[0]
+    sheet = wb.sheets[2]
     if sheet["A1"].value == "Hello xlwings!":
         sheet["A1"].value = "Bye xlwings!"
     else:
@@ -13,6 +13,13 @@ def main():
 @xw.func
 def hello(name):
     return f"Hello {name}!"
+
+
+@xw.sub
+def my_macro():
+    """Writes the name of the Workbook into Range("A1") of Sheet 1"""
+    wb = xw.Book.caller()
+    wb.sheets[3].range('A1').value = "Mirsoat"
 
 
 if __name__ == "__main__":
